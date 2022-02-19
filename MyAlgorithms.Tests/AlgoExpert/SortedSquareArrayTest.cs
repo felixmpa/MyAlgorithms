21c56cd057783b7fd3a6a9d6d0dcfa775e2e2015
= <=>
2022-02-19 11:23:48 -0400

    Sorted square array

---
+++ MyAlgorithms.Tests/AlgoExpert/SortedSquareArrayTest.cs
@@ -0,0 +1,38 @@
+﻿using MyAlgorithms.AlgoExpert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyAlgorithms.Tests.AlgoExpert
+{
+    public class SortedSquareArrayTest
+    {
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Method1(int[] arr, int[] expected)
+        {
+            int[] actual = SortedSquaredArray.Method1(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Method2(int[] arr, int[] expected)
+        {
+            int[] actual = SortedSquaredArray.Method2(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Data()
+        {
+            yield return new object[] { new int[2] { -2, -1}, new int[2] {1, 4} };
+            yield return new object[] { new int[1] { -10 }, new int[1] { 100 } };
+            yield return new object[] { new int[7] { 1, 2, 3, 5, 6, 8, 9 }, new int[7] {1, 4, 9, 25, 36, 64, 81} };
+        }
+
+    }
+}
---
+++ MyAlgorithms/AlgoExpert/SortedSquaredArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlgorithms.AlgoExpert
+{
+    public class SortedSquaredArray
+    {
+        // O(nLogn) time | O(n) space
+        public static int[] Method1(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= array[i];
+            }
+            Array.Sort(array);
+            return array;
+        }
+
+        // O(n) time | O(n) space
+         public static int[] Method2(int[] array)
+        {
+            int[] sorted_array = new int[array.Length];
+            int idxLeft  = 0;
+            int idxRight = array.Length - 1;
+            
+            for(int idx = array.Length - 1; idx >= 0; idx --)
+            { 
+                int leftValue  = array[idxLeft] * array[idxLeft];
+                int rightValue = array[idxRight] * array[idxRight];
+
+                if (leftValue > rightValue)
+                {
+                    sorted_array[idx] = leftValue;
+                    idxLeft++;
+                }else {
+                    sorted_array[idx] = rightValue;
+                    idxRight--;
+                }
+            }
+            return sorted_array;
+        }
+
+    }
+}
