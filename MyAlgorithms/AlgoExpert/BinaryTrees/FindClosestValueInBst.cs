2e3fdc0fe15733f3524fa48857132ba5d57b2a00
= <=>
2022-03-08 22:06:09 -0400

    Find Closest Value in BTST (Binary Search Tree)

--- MyAlgorithms/AlgoExpert/BinaryTrees/FindClosestValueInBst.cs
+++ MyAlgorithms/AlgoExpert/BinaryTrees/FindClosestValueInBst.cs
@@ -4,66 +4,71 @@ using System.Text;
 
 namespace MyAlgorithms.AlgoExpert.BinaryTrees
 {
-	public class BST
-	{
-		public int value;
-		public BST left;
-		public BST right;
-
-		public BST(int value)
-		{
-			this.value = value;
-		}
-	}
-
-	public class FindClosestValueInBst
+    public class BST
     {
-		public static  int Method1(BST tree, int target)
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
         {
-			int closest = tree.value;
-			var queue	= new Queue<BST>();
-			queue.Enqueue(tree);
+            this.value = value;
+        }
+    }
 
-			while(queue.Count > 0)
+    public class FindClosestValueInBst
+    {
+        public static int Method1(BST tree, int target)
+        {
+            int closest = tree.value;
+            var queue = new Queue<BST>();
+            queue.Enqueue(tree);
+
+            while (queue.Count > 0)
             {
-				var current = queue.Dequeue();
+                var current = queue.Dequeue();
 
-				if (current.left != null)
-					queue.Enqueue(current.left);
-				if (current.right != null)
-					queue.Enqueue(current.right);
+                if (current.left != null)
+                    queue.Enqueue(current.left);
+                if (current.right != null)
+                    queue.Enqueue(current.right);
 
-				if (Math.Abs(target - closest) > Math.Abs(target - current.value))
-					closest = current.value;
+                if (Math.Abs(target - closest) > Math.Abs(target - current.value))
+                    closest = current.value;
             }
 
-			return closest;
-		}
+            return closest;
+        }
 
-		/*
+        /*
 		 * RECURSIVE
 		 * FindClosestValueInBstRecursive(BST tree, int target)
 		 */
-		public static int Method2(BST tree, int target)
+        public static int Method2(BST tree, int target)
         {
-			return Method2(tree, target, tree.value);
+            return Method2(tree, target, tree.value);
         }
 
-		public static int Method2(BST tree, int target, int closest)
+        public static int Method2(BST tree, int target, int closest)
         {
-			if (Math.Abs(target - closest) > Math.Abs(target - tree.value))
-				closest = tree.value;
+            if (Math.Abs(target - closest) > Math.Abs(target - tree.value))
+                closest = tree.value;
 
-			if (target < tree.value && tree.left != null){
-				return Method2(tree.left, target, closest);
-            }else if( target > tree.value && tree.right != null){
-				return Method2(tree.right, target, closest);
-            }else{
-				return closest;
+            if (target < tree.value && tree.left != null)
+            {
+                return Method2(tree.left, target, closest);
+            }
+            else if (target > tree.value && tree.right != null)
+            {
+                return Method2(tree.right, target, closest);
+            }
+            else
+            {
+                return closest;
             }
         }
 
 
 
-	}
+    }
 }
