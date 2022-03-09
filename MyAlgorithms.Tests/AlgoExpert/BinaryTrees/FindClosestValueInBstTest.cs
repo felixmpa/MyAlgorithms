5d41ef245e5dd8110aa06b393e06903e3dc8ee8e
= <=>
2022-03-08 22:03:26 -0400

    Find Closest Value in BTST (Binary Search Tree)

---
+++ MyAlgorithms.Tests/AlgoExpert/BinaryTrees/FindClosestValueInBstTest.cs
@@ -0,0 +1,71 @@
+﻿using MyAlgorithms.AlgoExpert.BinaryTrees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyAlgorithms.Tests.AlgoExpert.BinaryTrees
+{
+    public class FindClosestValueInBstTest
+    {
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Method1(BST tree, int target, int expected)
+        {
+            int actual = FindClosestValueInBst.Method1(tree, target);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Method2(BST tree, int target, int expected)
+        {
+            int actual = FindClosestValueInBst.Method2(tree, target);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Data()
+        {
+
+            /* 
+             *tree = 10
+             *       /\
+             *     5   15
+             *    /\   /\
+             *   2  5 13 22   
+             *  /      \
+             * 1        24
+             * 
+             * target = 12
+             * output = 13
+             */
+            yield return new object[] {                 
+                new BST(10) 
+                { 
+                    left = new BST(5) { 
+                        left = new BST(2) {
+                           left = new BST(1)
+                        }, 
+                        right = new BST(5) 
+                    },
+
+                    right = new BST(15) {
+                        left = new BST(13) {
+                            right = new BST(14)
+                        },
+                        right =  new BST(22)
+                    } 
+                },
+                12,
+                13
+            };
+                
+
+
+        }
+
+    }
+}
---
+++ MyAlgorithms/AlgoExpert/BinaryTrees/FindClosestValueInBst.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlgorithms.AlgoExpert.BinaryTrees
+{
+	public class BST
+	{
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value)
+		{
+			this.value = value;
+		}
+	}
+
+	public class FindClosestValueInBst
+    {
+		public static  int Method1(BST tree, int target)
+        {
+			int closest = tree.value;
+			var queue	= new Queue<BST>();
+			queue.Enqueue(tree);
+
+			while(queue.Count > 0)
+            {
+				var current = queue.Dequeue();
+
+				if (current.left != null)
+					queue.Enqueue(current.left);
+				if (current.right != null)
+					queue.Enqueue(current.right);
+
+				if (Math.Abs(target - closest) > Math.Abs(target - current.value))
+					closest = current.value;
+            }
+
+			return closest;
+		}
+
+		/*
+		 * RECURSIVE
+		 * FindClosestValueInBstRecursive(BST tree, int target)
+		 */
+		public static int Method2(BST tree, int target)
+        {
+			return Method2(tree, target, tree.value);
+        }
+
+		public static int Method2(BST tree, int target, int closest)
+        {
+			if (Math.Abs(target - closest) > Math.Abs(target - tree.value))
+				closest = tree.value;
+
+			if (target < tree.value && tree.left != null){
+				return Method2(tree.left, target, closest);
+            }else if( target > tree.value && tree.right != null){
+				return Method2(tree.right, target, closest);
+            }else{
+				return closest;
+            }
+        }
+
+
+
+	}
+}
